bf5c07759d635725ba813982222f2edffe59e2e2
bip-zip <60959655+bip-zip@users.noreply.github.com>
2021-01-08 06:19:17 +0545

    Update settings.py

--- unicart/settings.py
+++ unicart/settings.py
@@ -141,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER='herohiralaal14@gmail.com'
-EMAIL_HOST_PASSWORD='Hero@@9813643009'
+EMAIL_HOST_PASSWORD='*******'
 EMAIL_USE_TLS = True
